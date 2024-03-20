085f54253fd3cf2df802e05f57d3c08457a82737
Benc8 <menyhert.bence08@gmail.com>
2024-03-20 15:55:18 +0100

    vazlat_1
    csutortokre nagyjabol meg van

--- src/main/main.js
+++ src/main/main.js
@@ -2,6 +2,7 @@ let isLightTheme = true;
 let body = document.getElementById("body");
 let nav = document.getElementById("nav");
 let css = document.getElementById("cssLink");
+let toggleSwitch = document.getElementById("mySwitch");
 
 function themeChange() {
     if (isLightTheme) {
@@ -16,6 +17,10 @@ function themeChange() {
     localStorage.setItem('theme', isLightTheme ? 'light' : 'dark');
 }
 
+function pageChange(url){
+    window.location.href = url;
+}
+
 //localstorage for theme change
 document.addEventListener('DOMContentLoaded', function() {
     const savedTheme = localStorage.getItem('theme');
@@ -27,6 +32,8 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             body.setAttribute('data-bs-theme', "dark");
             css.setAttribute('href', "main_dark.css");
+            toggleSwitch.checked = true;
+            
         }
     }
 });
